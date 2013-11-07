115e6d4717ac78d8ec2b28a0898bd4c6f0869b10
Andres Riancho <andres.riancho@gmail.com>
2013-11-07 18:46:29 -0300

    Initial commit of (almost) empty project for django-moth

---
+++ djmoth/djmoth/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+
+
+from django.contrib import admin
+admin.autodiscover()
+
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^/', include('moth.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
