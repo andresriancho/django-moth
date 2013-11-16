dcc3355c723165a303984f71bce6e1fe6e3ff310
Andres Riancho <andres.riancho@gmail.com>
2013-11-17 07:36:56 +0900

    Ignore order in result

--- moth/tests/test_plugin_families.py
+++ moth/tests/test_plugin_families.py
@@ -6,5 +6,5 @@ from moth.utils.plugin_families import get_plugin_families
 class TestPluginFamilies(unittest.TestCase):
     def test_get_plugin_families(self):
         result = get_plugin_families()
-        expected = ['grep', 'audit']
-        self.assertEqual(result, expected)
+        expected = set(['grep', 'audit'])
+        self.assertEqual(set(result), expected)
