68379c288f86266adb2e37d511ef0b7fc2a3d050
Andres Riancho <andres.riancho@gmail.com>
2013-11-17 18:44:07 +0900

    Fixing tests for newly added plugin family to url

--- moth/tests/test_router.py
+++ moth/tests/test_router.py
@@ -3,14 +3,14 @@ from django.test import TestCase
 
 class RouterTestCase(TestCase):
     def test_routes_grep(self):
-        response = self.client.get('/event_validation/event_validation.html')
+        response = self.client.get('/grep/event_validation/event_validation.html')
         
         self.assertIn('.NET Event validation', response.content)
         self.assertTemplateUsed(response, 'moth/base.html')
         self.assertTemplateUsed(response, 'moth/vulnerability.html')
     
     def test_index_of(self):
-        response = self.client.get('/event_validation/')
+        response = self.client.get('/grep/event_validation/')
         
         self.assertIn('Event Validation test suite', response.content)
         self.assertIn('(secure)', response.content)
