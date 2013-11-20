afebbef24f539bd839fde4bb1761601c97130fae
Andres Riancho <andres.riancho@gmail.com>
2013-11-20 11:21:09 +0100

    Adding tests for plugin family index

--- moth/tests/test_router.py
+++ moth/tests/test_router.py
@@ -20,3 +20,14 @@ class RouterTestCase(TestCase):
         self.assertTemplateUsed(response, 'moth/index-of.html')
         
         self.assertNotIn('cross_domain_script_with_type.html', response.content)
+
+    def test_family_index_of(self):
+        response = self.client.get('/grep/')
+        
+        self.assertIn('Event validation', response.content)
+        self.assertIn('DOM XSS', response.content)
+        
+        self.assertTemplateUsed(response, 'moth/base.html')
+        self.assertTemplateUsed(response, 'moth/index-of-family.html')
+        
+        self.assertNotIn('simple_xss.py', response.content)
