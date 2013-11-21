bebd95d8f880f5df8b4f13fefc1c1c6e2c4ef8b0
Andres Riancho <andres.riancho@gmail.com>
2013-11-21 08:27:20 +0100

    Fixing build

--- moth/utils/plugin_families.py
+++ moth/utils/plugin_families.py
@@ -6,6 +6,9 @@ def get_plugin_families():
     :return: A list of strings with the names of the plugin families.
     '''
     vuln_content = os.listdir('moth/views/vulnerabilities/')
-    vuln_content.remove('__init__.py')
-    vuln_content.remove('__init__.pyc')
+    try:
+        vuln_content.remove('__init__.py')
+        vuln_content.remove('__init__.pyc')
+    except ValueError:
+        pass
     return vuln_content
