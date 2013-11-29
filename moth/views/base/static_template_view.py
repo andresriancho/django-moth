9c0eb9b9790e1f1a234730f7358a2a70cd15c254
Andres Riancho <andres.riancho@gmail.com>
2013-11-29 13:30:42 -0300

    Minor refactoring to expose constant

--- moth/views/base/static_template_view.py
+++ moth/views/base/static_template_view.py
@@ -5,6 +5,9 @@ from django.http import HttpResponse
 from moth.views.base.vulnerable_template_view import VulnerableTemplateView
 
 
+STATIC_DIR = 'moth/static/moth/misc/'
+
+
 class StaticFileView(VulnerableTemplateView):
     '''
     When we want to return the content of a static file, which doesn't
@@ -14,13 +17,12 @@ class StaticFileView(VulnerableTemplateView):
     Files are read from the moth/static/misc/ directory.
     '''
     STATIC_FILE = None
-    STATIC_DIR = 'moth/static/moth/misc/'
     
     def get(self, request, *args, **kwds):
         if self.STATIC_FILE is None:
             raise RuntimeError('STATIC_FILE needs to be set.')
 
-        file_path = os.path.join(self.STATIC_DIR, self.STATIC_FILE)
+        file_path = os.path.join(STATIC_DIR, self.STATIC_FILE)
         content_type, _ = mimetypes.guess_type(self.STATIC_FILE)
         
         return HttpResponse(file(file_path).read(),
