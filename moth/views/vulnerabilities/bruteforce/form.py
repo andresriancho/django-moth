4051b8920093c814f585add958fdedd01ce6d2e6
Andres Riancho <andres.riancho@gmail.com>
2013-11-29 10:49:19 -0300

    Adding more forms to make sure the tests pass Also, added a * 300 as a workaround for https://github.com/andresriancho/w3af/issues/799

--- moth/views/vulnerabilities/bruteforce/form.py
+++ moth/views/vulnerabilities/bruteforce/form.py
@@ -6,7 +6,11 @@ from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit
 
-AUTH_SUCCESS = 'Authentication success, welcome!'
+#
+#    FIXME: See https://github.com/andresriancho/w3af/issues/799 to understand
+#           why we need the * 300
+#
+AUTH_SUCCESS = 'Authentication success, welcome!' * 300
 AUTH_ERROR = 'Invalid username and/or password'
 
 
@@ -35,6 +39,22 @@ class PostLoginForm(forms.Form):
 class GetLoginForm(PostLoginForm):
     METHOD = 'GET'
 
+class PasswordOnlyLoginForm(forms.Form):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    
+    def __init__(self, * args, **kwargs):
+        # pylint: disable=E1002
+        super(PasswordOnlyLoginForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-inline'
+        
+        submit = Submit('Submit', 'Submit', css_class="btn-success")
+
+        self.helper.layout = Layout(
+            'password',
+            submit,
+        )
 
 class PostGuessableCredsLoginFormView(FormTemplateView):
     description = title = 'Guessable credentials in form authentication'
@@ -42,11 +62,37 @@ class PostGuessableCredsLoginFormView(FormTemplateView):
     url_path = 'guessable_login_form.py'
     users = (('admin', '1234'),)
     form_klass = PostLoginForm
+    references = ['https://github.com/andresriancho/w3af/issues/799']
     
     def post(self, request, *args, **kwds):
         context = self.get_context_data()
         
         if (request.POST['uname'], request.POST['password']) in self.users:
+            context['message'] = AUTH_SUCCESS
+        else:
+            context['message'] = AUTH_ERROR
+        
+        return render(request, self.template_name, context)
+
+class PostImpossibleCredsLoginFormView(PostGuessableCredsLoginFormView):
+    description = title = 'Complex credentials in form authentication'
+    tags = ['impossible', 'not guessable', 'POST']
+    url_path = 'impossible.py'
+    users = (('admin', 'admin1ad0min2admin3admi0n4'),)
+
+class PasswordOnlyGuessableCredsLoginFormView(FormTemplateView):
+    title = 'Guessable credentials in form authentication'
+    description = 'This login form only requires a password'
+    tags = ['trivial', 'POST', 'password only']
+    url_path = 'guessable_pass_only.py'
+    passwords = ('1234',)
+    form_klass = PasswordOnlyLoginForm
+    references = ['https://github.com/andresriancho/w3af/issues/799']
+    
+    def post(self, request, *args, **kwds):
+        context = self.get_context_data()
+        
+        if request.POST['password'] in self.passwords:
             context['message'] = AUTH_SUCCESS
         else:
             context['message'] = AUTH_ERROR
@@ -59,6 +105,7 @@ class GetGuessableCredsLoginFormView(FormTemplateView):
     url_path = 'guessable_login_form_get.py'
     users = (('admin', 'admin'),)
     form_klass = GetLoginForm
+    references = ['https://github.com/andresriancho/w3af/issues/799']
     
     def get(self, request, *args, **kwargs):
         if 'uname' in request.GET and 'password' in request.GET:
