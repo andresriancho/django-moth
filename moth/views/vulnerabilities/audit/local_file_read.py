81f972542c243c046f9cbf811c6da682ca7d06e8
Andres Riancho <andres.riancho@gmail.com>
2013-11-29 13:30:54 -0300

    Adding arbitrary local file read

---
+++ moth/views/vulnerabilities/audit/local_file_read.py
@@ -0,0 +1,23 @@
+import os
+
+from django.shortcuts import render
+
+from moth.views.base.vulnerable_template_view import VulnerableTemplateView
+from moth.views.base.static_template_view import STATIC_DIR
+
+
+class TrivialLocalFileReadView(VulnerableTemplateView):
+    description = title = 'Arbitrary local file read'
+    tags = ['trivial', 'GET']
+    url_path = 'local_file_read.py?file=section.txt'
+    
+    def get(self, request, *args, **kwds):
+        context = self.get_context_data()
+        fname = os.path.join(STATIC_DIR, request.GET['file'])
+        
+        if os.path.exists(fname):
+            context['html'] = file(fname).read()
+        else:
+            context['html'] = 'Unknown file.'
+            
+        return render(request, self.template_name, context)
