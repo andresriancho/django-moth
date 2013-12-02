d6872d9af9d268d5717bbd0085615ca3dcb2a48a
Andres Riancho <andres.riancho@gmail.com>
2013-12-02 16:24:41 -0300

    Adding string sql injection

--- moth/views/vulnerabilities/audit/sql_injection.py
+++ moth/views/vulnerabilities/audit/sql_injection.py
@@ -8,7 +8,8 @@ from moth.views.base.vulnerable_template_view import VulnerableTemplateView
 
 class SQLIntegerFormView(FormTemplateView):
     template_name = "moth/vulnerability-users-table.html"
-    title = 'Trivial SQL injection: (WHERE|integer|form)'
+    title = 'Trivial SQL injection'
+    tags = ['WHERE', 'integer', 'form']
     description = 'Trivial SQL injection in a SQL query WHERE section, integer'\
                   ' field which is reachable using an HTML form with one'\
                   ' parameter.'
@@ -22,7 +23,9 @@ class SQLIntegerFormView(FormTemplateView):
                                             form=GenericForm())
         else:
             user_input = form.cleaned_data[GenericForm.INPUT]
-            db_error, users = sql_injection_where_integer(user_input)
+            query = "SELECT * FROM auth_user WHERE id = %s" % user_input
+            
+            db_error, users = get_users(query)
             
             context = self.get_context_data(db_error=db_error,
                                             users=users,
@@ -32,7 +35,8 @@ class SQLIntegerFormView(FormTemplateView):
         return render(request, self.template_name, context)
 
 class SQLIntegerQSView(VulnerableTemplateView):
-    title = 'Trivial SQL injection: (WHERE|integer|query-string)'
+    title = 'Trivial SQL injection'
+    tags = ['WHERE', 'integer', 'query-string']
     description = 'Trivial SQL injection in a SQL query WHERE section, integer'\
                   ' field which is reachable using a query string.'
     url_path = 'where_integer_qs.py?id=1'
@@ -40,7 +44,9 @@ class SQLIntegerQSView(VulnerableTemplateView):
     
     def get(self, request, *args, **kwds):
         user_input = request.GET['id']
-        db_error, users = sql_injection_where_integer(user_input)
+        query = "SELECT * FROM auth_user WHERE id = %s" % user_input
+        
+        db_error, users = get_users(query)
         
         context = self.get_context_data(db_error=db_error,
                                         users=users,
@@ -48,11 +54,29 @@ class SQLIntegerQSView(VulnerableTemplateView):
         
         return render(request, self.template_name, context)
 
-def sql_injection_where_integer(user_input):
-    users = None
-    db_error = None
+class SQLSingleQuoteStringQSView(VulnerableTemplateView):
+    title = 'Trivial SQL injection'
+    tags = ['WHERE', 'string', 'single-quote', 'query-string']
+    description = 'Trivial SQL injection in a SQL query WHERE section, single'\
+                  ' quote string field which is reachable using a query string.'
+    url_path = 'where_string_single_qs.py?uname=pablo'
+    template_name = "moth/vulnerability-users-table.html"
     
-    query = "SELECT * FROM auth_user WHERE id = %s" % user_input
+    def get(self, request, *args, **kwds):
+        user_input = request.GET['uname']
+        query = "SELECT * FROM auth_user WHERE username = '%s'" % user_input
+        
+        db_error, users = get_users(query)
+        
+        context = self.get_context_data(db_error=db_error,
+                                        users=users,
+                                        success=True)
+        
+        return render(request, self.template_name, context)
+
+def get_users(query):
+    db_error = None
+    users = []
     
     try:
         users_qs = User.objects.raw(query)
@@ -61,6 +85,3 @@ def sql_injection_where_integer(user_input):
         db_error = str(dbe)
     
     return db_error, users
-
-# String WHERE
-# query = "SELECT * FROM auth_user WHERE username = '%s'" % user_input
