8f58dc11bf4338ba731d0e1c8666681b6fb8adc4
Andres Riancho <andres.riancho@gmail.com>
2014-02-27 15:30:46 -0300

    * Adding tests for XSS * Adding generic form with two inputs

--- moth/forms/generic.py
+++ moth/forms/generic.py
@@ -13,10 +13,33 @@ class GenericForm(forms.Form):
         super(GenericForm, self).__init__(*args, **kwargs)
 
         self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-x'
+        self.helper.field_class = 'col-lg-x'
+
         submit = Submit('Submit', 'Submit', css_class="btn-success")
 
         # generate layout
-        self.helper.layout = Layout(
-                                    Field(self.INPUT,),
-                                    submit
-                                    )
+        self.helper.layout = Layout(Field(self.INPUT,),
+                                    submit)
+
+
+class TwoInputForm(forms.Form):
+    name = forms.CharField()
+    address = forms.CharField()
+
+    def __init__(self, * args, **kwargs):
+        # pylint: disable=E1002
+        super(TwoInputForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-x'
+        self.helper.field_class = 'col-lg-x'
+
+        submit = Submit('Submit', 'Submit', css_class="btn-success")
+
+        # generate layout
+        self.helper.layout = Layout(Field('name'),
+                                    Field('address'),
+                                    submit)
--- moth/views/vulnerabilities/audit/xss.py
+++ moth/views/vulnerabilities/audit/xss.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+import re
 
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+
+from moth.forms.generic import TwoInputForm
 from moth.views.base.vulnerable_template_view import VulnerableTemplateView
 from moth.views.base.form_template_view import FormTemplateView
 
@@ -49,3 +53,110 @@ class FalsePositiveCheck499View(VulnerableTemplateView):
         
         return render(request, self.template_name, context)
 
+
+class XSSWithCSPView(VulnerableTemplateView):
+    title = 'Cross-Site scripting with CSP enabled'
+    tags = ['CSP', 'GET']
+    description = 'Echo query string parameter to HTML without any encoding'\
+                  ' for new browsers CSP is enabled using response header.'
+    url_path = 'xss_with_safe_csp.py?text=1'
+
+    def get(self, request, *args, **kwds):
+        context = self.get_context_data()
+        context['html'] = request.GET['text']
+
+        response = render_to_response(self.template_name, context)
+        response['Content-Security-Policy'] = "default-src 'self'"
+
+        return response
+
+
+class XSSWithWeakCSPView(VulnerableTemplateView):
+    title = 'Cross-Site scripting with weak CSP'
+    tags = ['CSP', 'GET']
+    description = 'Echo query string parameter to HTML without any encoding'\
+                  ' for new browsers CSP is enabled using response header.'\
+                  ' The CSP policy is weak and does NOT protect the users.'
+    url_path = 'xss_with_weak_csp.py?text=1'
+
+    def get(self, request, *args, **kwds):
+        context = self.get_context_data()
+        context['html'] = request.GET['text']
+
+        response = render_to_response(self.template_name, context)
+        response['Content-Security-Policy'] = "script-src *;"
+
+        return response
+
+
+class BlacklistFilterXSSView(VulnerableTemplateView):
+    title = 'Cross-Site scripting with trivial blacklist filter'
+    tags = ['trivial', 'GET', 'blacklist']
+    description = 'Echo query string parameter to HTML without any encoding'\
+                  ' filter input by removing "script".'
+    url_path = 'script_blacklist_xss.py?text=1'
+
+    def get(self, request, *args, **kwds):
+        context = self.get_context_data()
+        context['html'] = request.GET['text'].replace('script', '')
+        return render(request, self.template_name, context)
+
+
+class BlacklistFilterInsensitiveXSSView(VulnerableTemplateView):
+    title = 'Cross-Site scripting with trivial blacklist filter'
+    tags = ['trivial', 'GET', 'blacklist']
+    description = 'Echo query string parameter to HTML without any encoding'\
+                  ' filter input by removing "script", the replacement is done'\
+                  ' in a case insensitive way.'
+    url_path = 'script_insensitive_blacklist_xss.py?text=1'
+
+    def get(self, request, *args, **kwds):
+        context = self.get_context_data()
+        repl = re.compile('script', re.IGNORECASE)
+        context['html'] = repl.sub('', request.GET['text'])
+        return render(request, self.template_name, context)
+
+
+class TwoInputFormXSSView(FormTemplateView):
+    title = 'Cross-Site scripting in form with two inputs'
+    tags = ['POST']
+    description = 'Echo form parameter to HTML without any encoding only if'\
+                  ' the other parameter was filled.'
+    url_path = 'two_inputs_form.py'
+    form_klass = TwoInputForm
+
+    def post(self, request, *args, **kwds):
+        context = self.get_context_data()
+
+        if request.POST['name']:
+            context['message'] = request.POST['address']
+        else:
+            context['message'] = 'Invalid name'
+
+        return render(request, self.template_name, context)
+
+
+class PersistentFormXSSView(FormTemplateView):
+    title = 'Cross-Site scripting in form'
+    tags = ['trivial', 'POST', 'persistent']
+    description = 'Receives the data from an HTML form, stores it in session'\
+                  ' and then displays it when issuing a GET without encoding.'
+    url_path = 'persistent_xss_form.py'
+
+    def post(self, request, *args, **kwds):
+        context = self.get_context_data()
+        context['message'] = 'Data saved to DB.'
+
+        request.session['header'] = request.POST['text']
+
+        return render(request, self.template_name, context)
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_klass()
+
+        context = self.get_context_data(form=form)
+
+        header = request.session.get('header', '')
+        context['header'] = header or 'No input data yet.'
+
+        return render(request, self.template_name, context)
