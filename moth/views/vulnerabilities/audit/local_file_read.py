ff30249332f11c1c689ff790697de20a2cf21ea9
Andres Riancho <andres.riancho@gmail.com>
2014-02-27 14:24:07 -0300

    Adding more tests

--- moth/views/vulnerabilities/audit/local_file_read.py
+++ moth/views/vulnerabilities/audit/local_file_read.py
@@ -21,3 +21,52 @@ class TrivialLocalFileReadView(VulnerableTemplateView):
             context['html'] = 'Error!'
             
         return render(request, self.template_name, context)
+
+
+class AppendExtensionLocalFileReadView(VulnerableTemplateView):
+    description = title = 'Arbitrary local file read with extension appended'
+    tags = ['GET']
+    url_path = 'local_file_read_append_extension.py?file=section'
+
+    def get(self, request, *args, **kwds):
+        context = self.get_context_data()
+        fname = os.path.join(STATIC_DIR, request.GET['file']) + '.txt'
+
+        try:
+            context['html'] = file(fname).read()
+        except:
+            context['html'] = 'Error!'
+
+        return render(request, self.template_name, context)
+
+
+class FullPathFileReadView(VulnerableTemplateView):
+    description = title = 'Full path arbitrary file read'
+    tags = ['trivial', 'GET']
+    url_path = 'local_file_read_full_path.py?file=section.txt'
+
+    def get(self, request, *args, **kwds):
+        context = self.get_context_data()
+
+        try:
+            context['html'] = file(request.GET['file']).read()
+        except:
+            context['html'] = 'Error!'
+
+        return render(request, self.template_name, context)
+
+
+class EchoPasswdView(VulnerableTemplateView):
+    description = title = 'Echo /etc/passwd file'
+    tags = ['GET']
+    url_path = 'false_positive_1.py?file=section.txt'
+    false_positive_check = True
+
+    def get(self, request, *args, **kwds):
+        context = self.get_context_data()
+
+        html = request.GET['file']
+        html += 'root:x:0:0:'
+        context['html'] = html
+
+        return render(request, self.template_name, context)
