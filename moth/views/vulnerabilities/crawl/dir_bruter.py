7b770e9026a7997e6faa0e479cccb45ce19b4e46
Andres Riancho <andres.riancho@gmail.com>
2014-03-25 14:28:27 -0300

    Changes required for Enable these tests @ CircleCI #1368

--- moth/views/vulnerabilities/crawl/dir_bruter.py
+++ moth/views/vulnerabilities/crawl/dir_bruter.py
@@ -1,12 +1,45 @@
 from moth.views.base.raw_path_view import RawPathTemplateView
+from moth.views.base.html_template_view import HTMLTemplateView
 
 
 class IAmHiddenView(RawPathTemplateView):
-    title = 'A hidden file'
+    title = 'A hidden file in the webroot'
     description = 'Simple unlinked file at /iamhidden.txt'
     url_path = 'iamhidden.txt'
     linked = False
     
     HTML = '''
     Please find me dir_bruter!
+    '''
+
+
+class HiddenDirectoryView(HTMLTemplateView):
+    title = 'A hidden directory'
+    description = 'Simple unlinked directory'
+    url_path = 'spameggs/'
+    linked = False
+
+    HTML = '''
+    I am a hidden directory!
+    '''
+
+
+class HiddenDirectoryView(HTMLTemplateView):
+    title = 'A hidden directory inside the spameggs directory'
+    description = 'Recursive test for directory inside directory'
+    url_path = 'spameggs/foobar/'
+    linked = False
+
+    HTML = '''
+    Double match! Found spameggs and then foobar inside it.
+    '''
+
+class HiddenFileView(HTMLTemplateView):
+    title = 'A hidden file'
+    description = 'Simple unlinked file inside the dir_bruter directory'
+    url_path = 'hidden-inside-dir.txt'
+    linked = False
+
+    HTML = '''
+    I am a hidden file!
     '''
