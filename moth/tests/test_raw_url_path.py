fdeebf34214accdb37028b75c07173e9c2ad849d
Andres Riancho <andres.riancho@gmail.com>
2014-03-27 15:08:48 -0300

    Fix pylint false positive

--- moth/tests/test_raw_url_path.py
+++ moth/tests/test_raw_url_path.py
@@ -5,5 +5,6 @@ class RawURLPathTestCase(TestCase):
     def test_raw_url(self):
         # Tests RawPathTemplateView
         response = self.client.get('/iamhidden.txt')
-        
+
+        # pylint: disable=E1103
         self.assertEqual(200, response.status_code)
