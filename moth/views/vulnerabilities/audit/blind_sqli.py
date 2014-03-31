99ab01c78be6b3d2706e80b38ce34e6fb0e25ffa
Andres Riancho <andres.riancho@gmail.com>
2014-03-31 18:35:25 -0300

    Added new test while debugging You have provided tainted parameter values to sqlmap #262

--- moth/views/vulnerabilities/audit/blind_sqli.py
+++ moth/views/vulnerabilities/audit/blind_sqli.py
@@ -2,6 +2,38 @@ from django.shortcuts import render
 
 from moth.views.base.vulnerable_template_view import VulnerableTemplateView
 from moth.views.vulnerabilities.audit.sql_injection import get_users
+from moth.views.base.form_template_view import FormTemplateView
+from moth.forms.generic import GenericForm
+
+
+class BlindSQLIntegerFormView(FormTemplateView):
+    template_name = "moth/vulnerability-users-table.html"
+    title = 'Trivial blind SQL injection in form'
+    tags = ['WHERE', 'integer', 'form', 'blind']
+    description = 'Trivial blind SQL injection in a SQL query WHERE section,'\
+                  ' integer field which is reachable using an HTML form with'\
+                  ' one parameter.'
+    url_path = 'blind_where_integer_form.py'
+
+    def post(self, request, *args, **kwds):
+        # pylint: disable=E1101
+        form = GenericForm(data=request.POST)
+        if not form.is_valid():
+            context = self.get_context_data(success=False,
+                                            form=GenericForm())
+        else:
+            user_input = form.cleaned_data[GenericForm.INPUT]
+            query = "SELECT * FROM auth_user WHERE id = %s" % user_input
+
+            db_error, users = get_users(query)
+            db_error, users = fake_error_handling(db_error, users)
+
+            context = self.get_context_data(db_error=db_error,
+                                            users=users,
+                                            success=True,
+                                            form=GenericForm())
+
+        return render(request, self.template_name, context)
 
 
 class BlindSQLIntegerQSView(VulnerableTemplateView):
