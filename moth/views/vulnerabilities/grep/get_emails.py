bfd8c73ecb45f332c0afa0ce72fa4723fb779980
Andres Riancho <andres.riancho@gmail.com>
2014-04-18 10:07:15 -0300

    Updated page to match test

--- moth/views/vulnerabilities/grep/get_emails.py
+++ moth/views/vulnerabilities/grep/get_emails.py
@@ -4,35 +4,28 @@ from moth.views.base.html_template_view import HTMLTemplateView
 class GetEmailsView(HTMLTemplateView):
     title = 'Page with lots of emails'
     description = 'Page with a ton of different emails, false positive checks'\
-                  ' email formats, encodings, separators, etc.'
+                  ' email formats, encodings, separators, etc. Keep in mind'\
+                  ' that w3af will only extract emails in "a" tags with'\
+                  ' "mailto:" protocol (for performance reasons).'
     url_path = 'emails.html'
     
     HTML = '''
     <html>
-    Test various email separator characters <br />
-    %20f00@moth.com
-                    bar@moth.com
-             hello@world.com
-    
-    <br/><br />Test URL encoded emails: <br />
-    hello%20world@f00.net
-    
-    <br/><br />Test email with invalid URL encode: <br />
-            hello%planer@moth.com
-    
-    <br/><br />Test email with invalid URL encode: <br />
-    andres@moth , please note that this email address won't be found because it
-    doesn't match the regular expression we use to find emails, which requires
-    the domain part to have at least a dot (.).
-    
-    <br /><br />Test mailto tag: <br />
-    %20%20%20%20<mailto href="pp@moth.com">
-    
-    <br /><br />Test misc:<br/>
-            notMe!@gmail.com
-            notMe@gmail.com pleaseMe@moth
-    
-    <br /><br />Test HTML encoded:<br/>
-    ramosmejia_direcci&oacute;n@moth
-    </html>
+    Simplest cases:<br />
+        <ul>
+            <li><a href="mailto:one@moth.com">test</a></li>
+            <li><a href="mailto:two@moth.com?subject=hello">test</a></li>
+        <ul>
+
+    Test various email separator characters:<br />
+        <ul>
+            <li><a href="mailto:%20three@moth.com">test</a></li>
+            <li><a href="mailto: four@moth.com">test</a></li>
+        <ul>
+
+    Test invalid email addresses:<br />
+        <ul>
+            <li><a href="mailto:false!@moth.com">test</a></li>
+            <li><a href="mailto:false@@moth.com">test</a></li>
+        <ul>
     '''
